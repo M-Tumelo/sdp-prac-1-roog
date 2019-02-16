9f799382954efaa5fef215838707e22644a04a1d
Tumelo <g17m9389@campus.ru.ac.za>
2019-02-16 17:43:46 +0200

    Roog updated

--- Roog/Program.cs
+++ Roog/Program.cs
@@ -7,6 +7,7 @@ namespace Roog
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Student Number: g17m9389");
         }
     }
 }
